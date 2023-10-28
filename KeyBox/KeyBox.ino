d544d78282bdc1981581aac039baafd7d6f3c7c3
hubert14 <huberttubeline@gmail.com>
2023-10-28 18:46:53 +0300

    Comment Serial output Add readme

--- KeyBox/KeyBox.ino
+++ KeyBox/KeyBox.ino
@@ -1,6 +1,7 @@
-#include <Joystick.h>
-#include <EncButton.h>
+#include <Joystick.h> // https://github.com/MHeironimus/ArduinoJoystickLibrary v2.1.1
+#include <EncButton.h> // https://github.com/GyverLibs/EncButton v2.0
 
+#define EB_NO_COUNTER
 #define PHYSICAL_BUTTONS_COUNT 5
 
 #define BUTTONS_COUNT 15
@@ -39,7 +40,6 @@ void setup() {
   }
 
   // Serial.begin(9600);
-  // Serial.println("start");
   Joystick.begin();
 }
 
@@ -73,16 +73,16 @@ void checkPots() {
   int pot_1_Val = analogRead(POT_1_PIN);
   if(pot_1_Val < previousPot_1_Val -2 || pot_1_Val > previousPot_1_Val + 2) {
     Joystick.setXAxis(pot_1_Val);
-    Serial.print("Pot1: ");
-    Serial.println(pot_1_Val);
+    // Serial.print("Pot1: ");
+    // Serial.println(pot_1_Val);
     previousPot_1_Val = pot_1_Val;
   }
 
   int pot_2_Val = analogRead(POT_2_PIN);
   if(pot_2_Val < previousPot_2_Val -2 || pot_2_Val > previousPot_2_Val + 2) {
     Joystick.setYAxis(pot_2_Val);
-    Serial.print("Pot2: ");
-    Serial.println(pot_2_Val);
+    // Serial.print("Pot2: ");
+    // Serial.println(pot_2_Val);
     previousPot_2_Val = pot_2_Val;
   }
 }
@@ -94,10 +94,10 @@ void checkButtons() {
     if (currentButtonState != lastButtonState[index])
     {
       Joystick.setButton(index, currentButtonState);
-      Serial.print("Button clicked | ");
-      Serial.print(currentButtonState);
-      Serial.print(" | ");
-      Serial.println(index);
+      // Serial.print("Button clicked | ");
+      // Serial.print(currentButtonState);
+      // Serial.print(" | ");
+      // Serial.println(index);
       lastButtonState[index] = currentButtonState;
     }
   }
@@ -145,7 +145,7 @@ void checkEncoders() {
       if(enc_holded[0]) {
         if(!enc_turned[0] && hold_val_1 > 600 && !enc_joy_state[0]) {
           Joystick.setButton(PHYSICAL_BUTTONS_COUNT + 4, 1);
-          Serial.print("hold");
+          // Serial.print("hold");
           enc_joy_state[0] = true;
         }
       }
@@ -154,37 +154,37 @@ void checkEncoders() {
       if(enc_holded[0]) {
         Joystick.setButton(PHYSICAL_BUTTONS_COUNT, 1);
         enc_1_millis[0] = current_millis;
-        Serial.println("LeftH trigger");
+        // Serial.println("LeftH trigger");
         enc_turned[0] = true;
       } else {
         Joystick.setButton(PHYSICAL_BUTTONS_COUNT + 1, 1);
         enc_1_millis[0] = current_millis;
-        Serial.println("left trigger");
+        // Serial.println("left trigger");
       }
       break;
     case 2:
       if(enc_holded[0]) {
         Joystick.setButton(PHYSICAL_BUTTONS_COUNT + 2, 1);
         enc_1_millis[2] = current_millis;
-        Serial.println("RightH trigger");
+        // Serial.println("RightH trigger");
         enc_turned[0] = true;
       } else {
         Joystick.setButton(PHYSICAL_BUTTONS_COUNT + 3, 1);
         enc_1_millis[3] = current_millis;
-        Serial.println("right trigger");
+        // Serial.println("right trigger");
       }
       break;
     case 3:
       enc_hold[0] = current_millis;
       enc_holded[0] = true;
-      Serial.println("press");
+      // Serial.println("press");
       break;
     case 4:
-      Serial.println("release");
+      // Serial.println("release");
       
       if(!enc_turned[0] && hold_val_1 < 350 && hold_val_1 > 70) {
           Joystick.setButton(PHYSICAL_BUTTONS_COUNT + 4, 1);
-          Serial.print("click");
+          // Serial.print("click");
         }
       
       enc_holded[0] = false;
@@ -216,7 +216,7 @@ void checkEncoders() {
       if(enc_holded[1]) {
         if(!enc_turned[1] && hold_val > 600 && !enc_joy_state[1]) {
           Joystick.setButton(PHYSICAL_BUTTONS_COUNT + 9, 1);
-          Serial.print("hold");
+          // Serial.print("hold");
           enc_joy_state[1] = true;
         }
       }
@@ -225,37 +225,37 @@ void checkEncoders() {
       if(enc_holded[1]) {
         Joystick.setButton(PHYSICAL_BUTTONS_COUNT + 5, 1);
         enc_2_millis[0] = current_millis;
-        Serial.println("LeftH trigger");
+        // Serial.println("LeftH trigger");
         enc_turned[1] = true;
       } else {
         Joystick.setButton(PHYSICAL_BUTTONS_COUNT + 6, 1);
         enc_2_millis[1] = current_millis;
-        Serial.println("left trigger");
+        // Serial.println("left trigger");
       }
       break;
     case 2:
       if(enc_holded[1]) {
         Joystick.setButton(PHYSICAL_BUTTONS_COUNT + 7, 1);
         enc_2_millis[2] = current_millis;
-        Serial.println("RightH trigger");
+        // Serial.println("RightH trigger");
         enc_turned[1] = true;
       } else {
         Joystick.setButton(PHYSICAL_BUTTONS_COUNT + 8, 1);
         enc_2_millis[3] = current_millis;
-        Serial.println("right trigger");
+        // Serial.println("right trigger");
       }
       break;
     case 3:
       enc_hold[1] = current_millis;
       enc_holded[1] = true;
-      Serial.println("press");
+      // Serial.println("press");
       break;
     case 4:
-      Serial.println("release");
+      // Serial.println("release");
       
       if(!enc_turned[1] && hold_val < 600 && hold_val > 70) {
           Joystick.setButton(PHYSICAL_BUTTONS_COUNT + 9, 1);
-          Serial.print("click");
+          // Serial.print("click");
         }
       
       enc_holded[1] = false;
---
+++ README.md
@@ -0,0 +1,95 @@
+# Install
+This project doesn`t have the libraries included in the project. 
+Need to install it by yourself.
+
+### Used libraries:
+- [Joystick](https://github.com/MHeironimus/ArduinoJoystickLibrary) v2.1.1
+- [EncButton](https://github.com/GyverLibs/EncButton) v2.0 (!  need to install exactly this version)
+
+### How to install libraries
+- Joystick:
+ - Download zip archive with code (from repository)
+ - In the Arduino IDE, select `Sketch` > `Include Library` > `Add .ZIP Library`
+- EncButton:
+ - Download it from the Library Manager inside the Arduino IDE `Tools` > `Manage Libraries` or `CTRL+SHIFT+I`
+
+
+# Schema
+- ### Buttons
+`D2`-`D6`
+
+- ### Encoders
+
+##### Left encoder
+
+| Pin  | Pin   |
+| ---- | ----  |
+| **CLK** | D9   |
+| **DT**   | D8   |
+| **SW**  | D10 |
+
+#####Right encoder
+
+| Pin  | Pin   |
+| ---- | ----  |
+| **CLK** | D15   |
+| **DT**   | D14   |
+| **SW**  | D16 |
+
+- ### Potentiometers
+`A0`, `A1`
+
+# Renaming device
+###### (works only on Arduino IDE 1.x)
+
+1. Open Arduino IDE folder (i.e. `C:\Program Files (x86)\Arduino`)
+2. Open `hardware\arduino\avr` folder
+3. Open `boards.txt` (possible require admin rights)
+4.  Add this after `Arduino Leonardo`:
+
+	```
+	#############################################################
+
+	menu.build_variant=Build Variant
+	leonardo.menu.build_variant.leonardo=Leonardo
+
+	leonardo.menu.build_variant.leonardo_switch=VitasyaKeyBox
+
+	leonardo.menu.build_variant.leonardo_switch.name=VitasyaKeyBox
+	leonardo.menu.build_variant.leonardo_switch.vid.0=0x0F0D
+	leonardo.menu.build_variant.leonardo_switch.pid.0=0x8092
+	leonardo.menu.build_variant.leonardo_switch.vid.1=0x0F0D
+	leonardo.menu.build_variant.leonardo_switch.pid.1=0x0092
+
+	leonardo.menu.build_variant.leonardo_switch.upload.tool=avrdude
+	leonardo.menu.build_variant.leonardo_switch.upload.protocol=avr109
+	leonardo.menu.build_variant.leonardo_switch.upload.maximum_size=28672
+	leonardo.menu.build_variant.leonardo_switch.upload.maximum_data_size=2560
+	leonardo.menu.build_variant.leonardo_switch.upload.speed=57600
+	leonardo.menu.build_variant.leonardo_switch.upload.disable_flushing=true
+	leonardo.menu.build_variant.leonardo_switch.upload.use_1200bps_touch=true
+	leonardo.menu.build_variant.leonardo_switch.upload.wait_for_upload_port=true
+
+	leonardo.menu.build_variant.leonardo_switch.bootloader.tool=avrdude
+	leonardo.menu.build_variant.leonardo_switch.bootloader.low_fuses=0xff
+	leonardo.menu.build_variant.leonardo_switch.bootloader.high_fuses=0xd8
+	leonardo.menu.build_variant.leonardo_switch.bootloader.extended_fuses=0xcb
+	leonardo.menu.build_variant.leonardo_switch.bootloader.file=caterina/Caterina-Leonardo.hex
+	leonardo.menu.build_variant.leonardo_switch.bootloader.unlock_bits=0x3F
+	leonardo.menu.build_variant.leonardo_switch.bootloader.lock_bits=0x2F
+
+	leonardo.menu.build_variant.leonardo_switch.build.mcu=atmega32u4
+	leonardo.menu.build_variant.leonardo_switch.build.f_cpu=16000000L
+	leonardo.menu.build_variant.leonardo_switch.build.vid=0x0F0D
+	leonardo.menu.build_variant.leonardo_switch.build.pid=0x0092
+	leonardo.menu.build_variant.leonardo_switch.build.usb_product="VITASYA KEYBOX"
+	leonardo.menu.build_variant.leonardo_switch.build.board=AVR_LEONARDO
+	leonardo.menu.build_variant.leonardo_switch.build.core=arduino
+	leonardo.menu.build_variant.leonardo_switch.build.variant=leonardo
+	leonardo.menu.build_variant.leonardo_switch.build.extra_flags={build.usb_flags}
+
+	leonardo.menu.build_variant.leonardo_switch.build.usb_manufacturer="Vitasya Tech"
+
+	#############################################################
+	```
+5. After that you will see the `Build Variant` after board selector
